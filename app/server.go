eb0fe0a8a4c1c20e7fc2a42c60a4809f3151c176
Yato03 <miguelhs3523@gmail.com>
2024-05-09 18:50:40 +0200

    concurrency

--- app/server.go
+++ app/server.go
@@ -9,7 +9,7 @@ import (
 	"github.com/codecrafters-io/http-server-starter-go/app/http"
 )
 
-func manageConnection(conn net.Conn) {
+func handleConnection(conn net.Conn) {
 
 	defer conn.Close()
 
@@ -40,23 +40,25 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage¡
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	for {
+		// Uncomment this block to pass the first stage¡
+		l, err := net.Listen("tcp", "0.0.0.0:4221")
 
-	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
-		os.Exit(1)
+		if err != nil {
+			fmt.Println("Failed to bind to port 4221")
+			os.Exit(1)
+		}
+
+		var conn net.Conn
+		conn, err = l.Accept()
+
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		//Accepted connection
+		go handleConnection(conn)
 	}
-
-	var conn net.Conn
-	conn, err = l.Accept()
-
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-
-	//Accepted connection
-	manageConnection(conn)
 
 }
